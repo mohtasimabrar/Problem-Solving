6b947ff63c8bf901fc7a58439c59c78054411571
Mohtasim Abrar Samin <50199254+mohtasimabrar@users.noreply.github.com>
2022-09-29 05:12:35 +0600

    Create 1_Two_Sum.swift

---
+++ 1_Two_Sum.swift
@@ -0,0 +1,13 @@
+class Solution {
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var ans: [Int]
+        for i in 0..<nums.count {
+            for j in i+1..<nums.count {
+                if nums[i] + nums[j] == target {
+                    return [i, j]
+                }
+            }
+        }
+        return []
+    }
+}
